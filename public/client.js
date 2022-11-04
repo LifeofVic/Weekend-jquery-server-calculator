d91210bf7546164aa2250ebbcc870362ab640732
Victor Llapa <victorllapa@gmail.com>
2022-11-03 21:54:37 -0500

    created a basic outline functions

--- public/client.js
+++ public/client.js
@@ -1,10 +1,37 @@
 $(document).ready(onReady);
 
-
+let compile = [];
 
 
 function onReady() {
+	$('#add-btn').on('click', addition);
+	$('#subtract-btn').on('click', subtraction);
+	$('#multiply-btn').on('click', multiply);
+	$('#divide-btn').on('click', divide);
 
+	$('#equal-btn').on('click', calculate);
 
 
 }
+
+
+function render() {
+	//will hold code that will keep the DOM up to date with the lastest input values (history results)
+}
+
+
+function addition() {
+	$('firstNumber').val()
+	//code to add
+}
+function subtraction() {
+	//code to subtract
+}
+function multiply() {
+	//code to multiply
+}
+function divide() {
+	//code to divide
+}
+
+
