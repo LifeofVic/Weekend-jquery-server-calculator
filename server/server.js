7b0c93da77f5526e351440ec4469e62a247e00fa
Victor Llapa <victorllapa@gmail.com>
2022-11-03 21:28:51 -0500

    created the basic code for the server.js

---
+++ server/server.js
@@ -0,0 +1,34 @@
+let express = require('expresss');
+app = express();
+
+const port = 5000;
+
+let bodyParser = require('body-Parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//will look in the 'public' folder for the files needed to display in the client side (DOM).
+app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+	console.log('Listening on port: ', port);
+});
