961b09ce45d5e4a249576f57791aae5e35865794
Victor Llapa <victorllapa@gmail.com>
2022-11-04 22:37:53 -0500

    readjusting the GET method in the client side

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-let express = require('expresss');
-app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const { response } = require('express');
 
 const port = 5000;
 
-let bodyParser = require('body-Parser');
-
-let resultCompiler = [];
+let resultCompiler = [{ data: 'it is working' }];
 let result = 0;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //will look in the 'public' folder for the files needed to display in the client side (DOM).
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 app.post('/result', (req, res) => {
 	let incomingData = req.body.newObject;
@@ -24,7 +24,12 @@ app.post('/result', (req, res) => {
 		result = Number(incomingData.firstValue - incomingData.secondValue);
 		incomingData.result = result;
 	}
+	resultCompiler.push(incomingData);
+	res.sendStatus(201);
+})
 
+app.get('/result', (req, res) => {
+	res.send(resultCompiler);
 })
 
 
